a7d007d2792ddc01286229d6637b5ddab6da7e52
jiakunxu <jiakunxu@outlook.com>
2013-07-11 15:12:56 +0800

    Reliance on default encoding

--- util/src/java/com/jiakun/xplatform/framework/util/EncryptUtil.java
+++ util/src/java/com/jiakun/xplatform/framework/util/EncryptUtil.java
@@ -21,12 +21,12 @@ import org.apache.log4j.Logger;
  */
 public final class EncryptUtil {
 
-	public static final String CHARSET_UTF8 = "UTF-8";
-
 	private static final Logger logger = Logger.getLogger(EncryptUtil.class);
 
 	private static final String[] HEX_DIGITS = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d",
 		"e", "f" };
+
+	private static final String CHARSET_UTF8 = "UTF-8";
 
 	private EncryptUtil() {
 
